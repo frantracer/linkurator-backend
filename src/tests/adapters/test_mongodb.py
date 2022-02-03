ab6086659f6988b2f3cb2b26235032d6be4be1f8
frantracer <frantracer@gmail.com>
2022-02-03 08:02:14 +0100

    Add test for get items by subscription

--- src/tests/adapters/test_mongodb.py
+++ src/tests/adapters/test_mongodb.py
@@ -246,3 +246,36 @@ def test_delete_item(item_repo: MongoDBItemRepository):
     item_repo.delete(item.uuid)
     deleted_item = item_repo.get(item.uuid)
     assert deleted_item is None
+
+
+def test_get_items_by_subscription_uuid(item_repo: MongoDBItemRepository):
+    subscription_uuid_1 = uuid.UUID("6ae3792e-6427-4b61-bdc1-66cc9c61fe29")
+    subscription_uuid_2 = uuid.UUID("d1dc868b-598c-4547-92d6-011e9b7e38e6")
+    subscription_uuid_3 = uuid.UUID("9753d304-3a43-414e-a5cd-496672b27c34")
+
+    item1 = Item(name="item1", uuid=uuid.UUID("6469596f-5128-4c12-87f1-9b7b462517f3"),
+                 subscription_uuid=subscription_uuid_1,
+                 url=utils.parse_url('https://test.com'),
+                 thumbnail=utils.parse_url('https://test.com/thumbnail.png'),
+                 created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
+    item2 = Item(name="item2", uuid=uuid.UUID("2dcc5c6b-3d95-421d-a9cf-81ac475cee4c"),
+                 subscription_uuid=subscription_uuid_1,
+                 url=utils.parse_url('https://test.com'),
+                 thumbnail=utils.parse_url('https://test.com/thumbnail.png'),
+                 created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
+    item3 = Item(name="item3", uuid=uuid.UUID("2f7fa436-e1db-4ac7-bcce-6299c246e39f"),
+                 subscription_uuid=subscription_uuid_2,
+                 url=utils.parse_url('https://test.com'),
+                 thumbnail=utils.parse_url('https://test.com/thumbnail.png'),
+                 created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
+    item_repo.add(item1)
+    item_repo.add(item2)
+    item_repo.add(item3)
+
+    items_from_sub1 = item_repo.get_by_subscription_id(subscription_uuid_1)
+    items_from_sub2 = item_repo.get_by_subscription_id(subscription_uuid_2)
+    items_from_sub3 = item_repo.get_by_subscription_id(subscription_uuid_3)
+
+    assert len(items_from_sub1) == 2
+    assert len(items_from_sub2) == 1
+    assert len(items_from_sub3) == 0
