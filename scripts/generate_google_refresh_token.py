924583394dfc5e2028919481f38d95808b83e4df
frantracer <frantracer@gmail.com>
2022-04-18 12:44:11 +0200

    Add scripts to test google authentication service

---
+++ scripts/generate_google_refresh_token.py
@@ -0,0 +1,48 @@
+import json
+import pathlib
+from urllib.parse import parse_qs, urlparse
+
+from linkurator_core.infrastructure.google.account_service import GoogleAccountService
+
+
+def main():
+    secret_path = f'{pathlib.Path(__file__).parent.absolute()}/../client_secret.json'
+    with open(secret_path, "r", encoding='UTF-8') as secrets_file:
+        secrets = json.loads(secrets_file.read())
+    client_id = secrets["web"]["client_id"]
+    client_secret = secrets["web"]["client_secret"]
+
+    google_account_service = GoogleAccountService(client_id=client_id, client_secret=client_secret)
+    redirect_uri = "http://localhost:9000"
+
+    auth_url = google_account_service.authorization_url(["openid", "profile", "email"], redirect_uri)
+
+    print(f'Please visit the following URL to authorize the application:\n {auth_url}')
+    print('\nAfter authorization, copy the redirected URL and paste it here:')
+    redirect_url_str = input()
+
+    url = urlparse(redirect_url_str)
+    code = parse_qs(url.query)["code"][0]
+    print(f'\nCode: {code}')
+
+    tokens = google_account_service.validate_code(code, redirect_uri)
+    if tokens is not None and tokens.refresh_token is None:
+        google_account_service.revoke_credentials(tokens.access_token)
+
+        print(f'\nYour credentials were revoke. Please log in again using the following URL:\n {auth_url}')
+        print('\nAfter authorization, copy the redirected URL and paste it here:')
+        redirect_url_str = input()
+
+        url = urlparse(redirect_url_str)
+        code = parse_qs(url.query)["code"][0]
+
+        tokens = google_account_service.validate_code(code, redirect_uri)
+
+    if tokens is not None and tokens.refresh_token is not None:
+        print(f'\nRefresh token: {tokens.refresh_token}')
+    else:
+        print('\nSomething went wrong. Please try again.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/get_user_info_from_refresh_token.py
@@ -0,0 +1,33 @@
+import argparse
+import json
+import pathlib
+
+from linkurator_core.infrastructure.google.account_service import GoogleAccountService
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Get google account user information')
+    parser.add_argument('--refresh-token', type=str, required=True,
+                        help='Refresh token that will be used to get the Access token')
+    args = parser.parse_args()
+
+    refresh_token = args.refresh_token
+
+    secret_path = f'{pathlib.Path(__file__).parent.absolute()}/../client_secret.json'
+    with open(secret_path, "r", encoding='UTF-8') as secrets_file:
+        secrets = json.loads(secrets_file.read())
+    client_id = secrets["web"]["client_id"]
+    client_secret = secrets["web"]["client_secret"]
+
+    google_account_service = GoogleAccountService(client_id=client_id, client_secret=client_secret)
+
+    access_token = google_account_service.generate_access_token_from_refresh_token(refresh_token)
+
+    user_info = google_account_service.get_user_info(access_token)
+
+    print(user_info)
+
+
+if __name__ == '__main__':
+    main()
