f06121018fd8ab415e091e456e178ec39e165585
frantracer <frantracer@gmail.com>
2022-06-07 18:30:23 +0200

    Add scheduler and event handler in the main function

--- linkurator_core/__main__.py
+++ linkurator_core/__main__.py
@@ -1,16 +1,27 @@
 import argparse
 import asyncio
-import signal
 from dataclasses import dataclass
+import os
+import signal
 from typing import Optional
 
-import uvicorn.server  # type: ignore
 from uvicorn import Config, Server  # type: ignore
+import uvicorn.server  # type: ignore
 
+from linkurator_core.application.find_outdated_users_handler import FindOutdatedUsersHandler
+from linkurator_core.infrastructure.asyncio.scheduler import TaskScheduler
+from linkurator_core.application.event_handler import EventHandler
+from linkurator_core.application.update_user_subscriptions_handler import UpdateUserSubscriptionsHandler
+from linkurator_core.domain.event import UserSubscriptionsBecameOutdatedEvent
 from linkurator_core.infrastructure.asyncio.event_bus_service import AsyncioEventBusService
 from linkurator_core.infrastructure.asyncio.utils import run_parallel
+from linkurator_core.infrastructure.config.google_secrets import GoogleClientSecrets
 from linkurator_core.infrastructure.config.mongodb import MongoDBSettings
+from linkurator_core.infrastructure.google.account_service import GoogleAccountService
+from linkurator_core.infrastructure.google.youtube_service import YoutubeService
 from linkurator_core.infrastructure.mongodb.repositories import run_mongodb_migrations
+from linkurator_core.infrastructure.mongodb.subscription_repository import MongoDBSubscriptionRepository
+from linkurator_core.infrastructure.mongodb.user_repository import MongoDBUserRepository
 
 
 @dataclass
@@ -28,18 +39,50 @@ class Settings:
     db: MongoDBSettings  # pylint: disable=invalid-name
 
 
-async def main():
+async def main():  # pylint: disable=too-many-locals
     settings = Settings(
         api=parse_args(),
         db=MongoDBSettings()
     )
 
+    # Migrations
     run_mongodb_migrations(settings.db.address, settings.db.port, settings.db.db_name, settings.db.user,
                            settings.db.password)
 
+    # Repositories
+    db_settings = MongoDBSettings()
+    user_repository = MongoDBUserRepository(ip=db_settings.address, port=db_settings.port, db_name=db_settings.db_name,
+                                            username=db_settings.user, password=db_settings.password)
+    subscription_repository = MongoDBSubscriptionRepository(
+        ip=db_settings.address, port=db_settings.port, db_name=db_settings.db_name,
+        username=db_settings.user, password=db_settings.password
+    )
+
+    # Services
+    google_client_secret_path = os.environ.get('LINKURATOR_GOOGLE_SECRET_PATH', "secrets/client_secret.json")
+    google_secrets = GoogleClientSecrets(google_client_secret_path)
+    account_service = GoogleAccountService(
+        client_id=google_secrets.client_id,
+        client_secret=google_secrets.client_secret)
+    youtube_service = YoutubeService(google_account_service=account_service, user_repository=user_repository)
+
+    # Event bus
     loop = asyncio.get_event_loop()
     event_bus = AsyncioEventBusService()
 
+    # Event handlers
+    update_user_subscriptions = UpdateUserSubscriptionsHandler(
+        youtube_service, user_repository, subscription_repository)
+    find_outdated_users = FindOutdatedUsersHandler(user_repository, event_bus)
+    event_handler = EventHandler(update_user_subscriptions)
+
+    event_bus.subscribe(UserSubscriptionsBecameOutdatedEvent, event_handler.handle)
+
+    # Task scheduler
+    scheduler = TaskScheduler()
+    scheduler.schedule_recurring_task(task=find_outdated_users.handle, interval_seconds=60 * 5)
+
+    # API
     api_server = ApiServer(app_path='linkurator_core.infrastructure.fastapi.app:app',
                            port=settings.api.port, workers=settings.api.workers, debug=settings.api.debug,
                            reload=settings.api.reload, with_gunicorn=settings.api.with_gunicorn)
@@ -47,10 +90,17 @@ async def main():
     for signal_type in [signal.SIGINT, signal.SIGTERM]:
         loop.add_signal_handler(
             signal_type,
-            lambda: loop.create_task(run_parallel(api_server.stop(), event_bus.stop()))
+            lambda: loop.create_task(run_parallel(
+                api_server.stop(),
+                event_bus.stop(),
+                scheduler.stop()))
         )
 
-    await run_parallel(event_bus.start(), api_server.start())
+    await run_parallel(
+        event_bus.start(),
+        api_server.start(),
+        scheduler.start()
+    )
 
 
 def parse_args() -> ApiArguments:
