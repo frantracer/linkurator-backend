a5dcb20855a31bddbc8397a3d844938394a74823
frantracer <frantracer@gmail.com>
2022-09-01 09:54:07 +0200

    Improve performance of update subscription items

--- linkurator_core/application/update_subscription_items_handler.py
+++ linkurator_core/application/update_subscription_items_handler.py
@@ -1,7 +1,10 @@
-from datetime import datetime, timezone
+import logging
 import uuid
+from datetime import datetime, timezone
+from typing import List
 
 from linkurator_core.application.subscription_service import SubscriptionService
+from linkurator_core.domain.item import Item
 from linkurator_core.domain.item_repository import ItemRepository
 from linkurator_core.domain.subscription_repository import SubscriptionRepository
 
@@ -21,20 +24,34 @@ class UpdateSubscriptionItemsHandler:
     async def handle(self, subscription_id: uuid.UUID) -> None:
         subscription = self.subscription_repository.get(subscription_id)
         if subscription is None:
-            print(f"Cannot update items of subscription {subscription_id} because it does not exist")
+            logging.error("Cannot update items of subscription %s because it does not exist", subscription_id)
             return
+
+        logging.info("Updating items for subscription %s - %s", subscription_id, subscription.name)
 
         now = datetime.now(tz=timezone.utc)
 
-        new_items = await self.subscription_service.get_items(
-            sub_id=subscription_id,
-            from_date=subscription.scanned_at)
-        for item in new_items:
-            found_item = self.item_repository.find(item)
-            if found_item is None:
-                self.item_repository.add(item)
+        try:
+            new_items = await self.subscription_service.get_items(
+                sub_id=subscription_id,
+                from_date=subscription.scanned_at)
+            item_count = 0
+            new_filtered_items: List[Item] = []
+            for item in new_items:
+                if item_count % 100 == 0:
+                    logging.debug("%s items processed", item_count)
+                item_count += 1
 
-        subscription.scanned_at = now
-        self.subscription_repository.update(subscription)
+                found_item = self.item_repository.find(item)
+                if found_item is None:
+                    new_filtered_items.append(item)
 
-        print(f"Updated items of subscription {subscription_id}")
+            self.item_repository.add_bulk(new_filtered_items)
+
+            subscription.scanned_at = now
+            self.subscription_repository.update(subscription)
+
+            logging.info("Updated %s items of subscription %s - %s",
+                         len(new_filtered_items), subscription.uuid, subscription.name)
+        except Exception as err:  # pylint: disable=broad-except
+            logging.error("Cannot update items of subscription %s because %s", subscription_id, err)
--- tests/unit/test_update_subscription_items_handler.py
+++ tests/unit/test_update_subscription_items_handler.py
@@ -5,14 +5,17 @@ import uuid
 
 import pytest
 
+from linkurator_core.application.subscription_service import SubscriptionService
 from linkurator_core.application.update_subscription_items_handler import UpdateSubscriptionItemsHandler
 from linkurator_core.common.utils import parse_url
 from linkurator_core.domain.item import Item
+from linkurator_core.domain.item_repository import ItemRepository
 from linkurator_core.domain.subscription import Subscription
+from linkurator_core.domain.subscription_repository import SubscriptionRepository
 
 
 @pytest.mark.asyncio
-async def test_update_subscriptions_items_with_a_items_that_are_not_registered():
+async def test_update_subscriptions_items_with_an_item_that_is_not_registered():
     sub1 = Subscription(
         uuid=uuid.UUID("b6cae596-4526-4ab7-b5da-bd803f04980b"),
         name="sub1",
@@ -33,13 +36,13 @@ async def test_update_subscriptions_items_with_a_items_that_are_not_registered()
         subscription_uuid=sub1.uuid,
         published_at=datetime.fromtimestamp(0, tz=timezone.utc))
 
-    subscription_service = AsyncMock()
+    subscription_service = AsyncMock(spec=SubscriptionService)
     subscription_service.get_items.return_value = [item1]
 
-    subscription_repository = MagicMock()
+    subscription_repository = MagicMock(spec=SubscriptionRepository)
     subscription_repository.get.return_value = copy(sub1)
 
-    item_repository = MagicMock()
+    item_repository = MagicMock(spec=ItemRepository)
     item_repository.find.return_value = None
 
     handler = UpdateSubscriptionItemsHandler(subscription_service=subscription_service,
@@ -53,8 +56,8 @@ async def test_update_subscriptions_items_with_a_items_that_are_not_registered()
     assert subscription_repository.get.call_count == 1
     assert subscription_repository.get.call_args == call(sub1.uuid)
     assert item_repository.find.call_count == 1
-    assert item_repository.add.call_count == 1
-    assert item_repository.add.call_args == call(item1)
+    assert item_repository.add_bulk.call_count == 1
+    assert item_repository.add_bulk.call_args == call([item1])
     assert subscription_repository.update.call_count == 1
     updated_sub = subscription_repository.update.call_args[0][0]
     assert updated_sub.scanned_at > sub1.scanned_at
