d32a705a00ee3733b9c260be6440e2f736094dbb
frantracer <frantracer@gmail.com>
2023-08-03 00:50:57 +0200

    Add handler to get user external credentials

---
+++ linkurator_core/application/users/get_user_external_credentials.py
@@ -0,0 +1,13 @@
+from typing import List
+from uuid import UUID
+
+from linkurator_core.domain.users.external_service_credential import ExternalServiceCredential
+from linkurator_core.domain.users.external_service_credential_repository import ExternalCredentialRepository
+
+
+class GetUserExternalCredentialsHandler:
+    def __init__(self, credentials_repository: ExternalCredentialRepository):
+        self.credentials_repository = credentials_repository
+
+    async def handle(self, user_uuid: UUID) -> List[ExternalServiceCredential]:
+        return await self.credentials_repository.get(user_uuid)
