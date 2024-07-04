efe2538b7797bdbb5c8be4c212d71245e40f331d
frantracer <10522656+frantracer@users.noreply.github.com>
2024-07-04 09:50:46 +0200

    Email sender using Gmail API

--- linkurator_core/infrastructure/config/google_secrets.py
+++ linkurator_core/infrastructure/config/google_secrets.py
@@ -6,16 +6,24 @@ class GoogleClientSecrets:
     client_id: str
     client_secret: str
     api_keys: list[str]
+    gmail_refresh_token: str
 
-    def __init__(self, file_path: str = '', api_key_path: str = '') -> None:
+    def __init__(self, file_path: str = '', api_key_path: str = '', gmail_refresh_token_path: str = '') -> None:
+        current_path = pathlib.Path(__file__).parent.absolute()
+
         if file_path == '':
-            file_path = f'{pathlib.Path(__file__).parent.absolute()}/../../../secrets/client_secret.json'
+            file_path = f'{current_path}/../../../secrets/client_secret.json'
         with open(file_path, "r", encoding='UTF-8') as secrets_file:
             secrets = json.loads(secrets_file.read())
         self.client_id = secrets["web"]["client_id"]
         self.client_secret = secrets["web"]["client_secret"]
 
         if api_key_path == '':
-            api_key_path = f'{pathlib.Path(__file__).parent.absolute()}/../../../secrets/google_api_key.txt'
+            api_key_path = f'{current_path}/../../../secrets/google_api_key.txt'
         with open(api_key_path, "r", encoding='UTF-8') as api_key_file:
             self.api_keys = [key for key in api_key_file.read().splitlines() if key.strip() != '']
+
+        if gmail_refresh_token_path == '':
+            gmail_refresh_token_path = f'{current_path}/../../../secrets/gmail_refresh_token.txt'
+        with open(gmail_refresh_token_path, "r", encoding='UTF-8') as gmail_refresh_token_file:
+            self.gmail_refresh_token = gmail_refresh_token_file.read().strip()
--- scripts/generate_google_refresh_token.py
+++ scripts/generate_google_refresh_token.py
@@ -1,16 +1,45 @@
+import argparse
+from dataclasses import dataclass
+from enum import Enum
 from urllib.parse import parse_qs, urlparse
 
 from linkurator_core.infrastructure.config.google_secrets import GoogleClientSecrets
 from linkurator_core.infrastructure.google.account_service import GoogleAccountService
 
 
+class Scopes(Enum):
+    YOUTUBE = "YOUTUBE"
+    GMAIL = "GMAIL"
+
+
+@dataclass
+class Arguments:
+    scopes: Scopes
+
+
+def parse_arguments() -> Arguments:
+    parser = argparse.ArgumentParser(description='Generate a Google refresh token')
+    parser.add_argument('--scope', choices=[field.value for field in list(Scopes)], default=Scopes.YOUTUBE,
+                        help='The scopes to request the refresh token for')
+
+    args = parser.parse_args()
+    return Arguments(scopes=Scopes(args.scope))
+
+
 def main() -> None:
+    arguments = parse_arguments()
+
     google_secrets = GoogleClientSecrets()
     google_account_service = GoogleAccountService(client_id=google_secrets.client_id,
                                                   client_secret=google_secrets.client_secret)
-    redirect_uri = "https://localhost:9000/auth"
+    redirect_uri = "http://localhost:9000/login_auth"
 
-    scopes = ["openid", "profile", "email", "https://www.googleapis.com/auth/youtube.readonly"]
+    scopes = ["openid", "profile", "email"]
+    if arguments.scopes == Scopes.GMAIL:
+        scopes = scopes + ["https://www.googleapis.com/auth/gmail.send"]
+    elif arguments.scopes == Scopes.YOUTUBE:
+        scopes = scopes + ["https://www.googleapis.com/auth/youtube"]
+
     auth_url = google_account_service.authorization_url(scopes, redirect_uri)
 
     print(f'Please visit the following URL to authorize the application:\n {auth_url}')
