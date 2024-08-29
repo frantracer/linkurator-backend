c74b38bf6040273d727bfe83bce5f3b631bbc52d
frantracer <10522656+frantracer@users.noreply.github.com>
2024-08-29 18:04:58 +0200

    Return error if trying to follow no existing subscription

--- linkurator_core/application/subscriptions/follow_subscription_handler.py
+++ linkurator_core/application/subscriptions/follow_subscription_handler.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from linkurator_core.domain.common.exceptions import UserNotFoundError
+from linkurator_core.domain.common.exceptions import UserNotFoundError, SubscriptionNotFoundError
 from linkurator_core.domain.subscriptions.subscription_repository import SubscriptionRepository
 from linkurator_core.domain.users.user_repository import UserRepository
 
@@ -14,6 +14,10 @@ class FollowSubscriptionHandler:
         user = await self.user_repository.get(user_id)
         if user is None:
             raise UserNotFoundError(user_id)
+
+        sub = await self.subscription_repository.get(subscription_id)
+        if sub is None:
+            raise SubscriptionNotFoundError(subscription_id)
 
         user.follow_subscription(subscription_id)
 
---
+++ tests/unit/test_follow_subscription_handler.py
@@ -0,0 +1,48 @@
+import pytest
+
+from linkurator_core.application.subscriptions.follow_subscription_handler import FollowSubscriptionHandler
+from linkurator_core.domain.common.exceptions import SubscriptionNotFoundError
+from linkurator_core.domain.common.mock_factory import mock_sub, mock_user
+from linkurator_core.infrastructure.in_memory.subscription_repository import InMemorySubscriptionRepository
+from linkurator_core.infrastructure.in_memory.user_repository import InMemoryUserRepository
+
+
+@pytest.mark.asyncio
+async def test_follow_subscription() -> None:
+    user_repo = InMemoryUserRepository()
+    subscription_repo = InMemorySubscriptionRepository()
+
+    sub = mock_sub()
+    user = mock_user()
+
+    await user_repo.add(user)
+    await subscription_repo.add(sub)
+
+    handler = FollowSubscriptionHandler(
+        subscription_repository=subscription_repo,
+        user_repository=user_repo
+    )
+
+    await handler.handle(user.uuid, sub.uuid)
+
+    updated_user = await user_repo.get(user.uuid)
+    assert updated_user is not None
+    assert sub.uuid in updated_user.get_subscriptions()
+
+
+@pytest.mark.asyncio
+async def test_follow_subscription_that_does_not_exist() -> None:
+    user_repo = InMemoryUserRepository()
+    subscription_repo = InMemorySubscriptionRepository()
+
+    user = mock_user()
+
+    await user_repo.add(user)
+
+    handler = FollowSubscriptionHandler(
+        subscription_repository=subscription_repo,
+        user_repository=user_repo
+    )
+
+    with pytest.raises(SubscriptionNotFoundError):
+        await handler.handle(user.uuid, user.uuid)
