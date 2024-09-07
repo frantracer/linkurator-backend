aa8cd43ab1262729c3ac79699a0fc593e7ef3bcc
frantracer <10522656+frantracer@users.noreply.github.com>
2024-09-07 17:43:48 +0200

    Refactor test get curator topics

--- tests/unit/test_get_curator_topics_as_user_handler.py
+++ tests/unit/test_get_curator_topics_as_user_handler.py
@@ -1,5 +1,4 @@
 from unittest.mock import AsyncMock
-from uuid import UUID
 
 import pytest
 
@@ -7,33 +6,24 @@ from linkurator_core.application.topics.get_curator_topics_as_user_handler impor
 from linkurator_core.domain.common.mock_factory import mock_user, mock_topic
 from linkurator_core.domain.topics.followed_topic import FollowedTopic
 from linkurator_core.domain.topics.followed_topics_repository import FollowedTopicsRepository
-from linkurator_core.domain.topics.topic_repository import TopicRepository
-from linkurator_core.domain.users.user import User
-from linkurator_core.domain.users.user_repository import UserRepository
+from linkurator_core.infrastructure.in_memory.topic_repository import InMemoryTopicRepository
+from linkurator_core.infrastructure.in_memory.user_repository import InMemoryUserRepository
 
 
 @pytest.mark.asyncio
 async def test_get_curator_topics_as_user_handler() -> None:
-    user_repo_mock = AsyncMock(spec=UserRepository)
+    user_repo_mock = InMemoryUserRepository()
     curator = mock_user()
     user = mock_user()
+    await user_repo_mock.add(curator)
+    await user_repo_mock.add(user)
 
-    async def mock_get_user(user_id: UUID) -> User | None:
-        if user_id == curator.uuid:
-            return curator
-        if user_id == user.uuid:
-            return user
-        return None
+    mocked_topic_1 = mock_topic(user_uuid=curator.uuid)
+    mocked_topic_2 = mock_topic(user_uuid=curator.uuid)
+    topic_repo_mock = InMemoryTopicRepository()
+    await topic_repo_mock.add(mocked_topic_1)
+    await topic_repo_mock.add(mocked_topic_2)
 
-    user_repo_mock.get = AsyncMock(side_effect=mock_get_user)
-
-    mocked_topic_1 = mock_topic()
-    mocked_topic_2 = mock_topic()
-    topic_repo_mock = AsyncMock(spec=TopicRepository)
-    topic_repo_mock.get_by_user_id.return_value = [
-        mocked_topic_1,
-        mocked_topic_2
-    ]
     followed_topics_repo_mock = AsyncMock(spec=FollowedTopicsRepository)
     followed_topics_repo_mock.get_followed_topics.return_value = [
         FollowedTopic.new(user_uuid=user.uuid, topic_uuid=mocked_topic_1.uuid)
