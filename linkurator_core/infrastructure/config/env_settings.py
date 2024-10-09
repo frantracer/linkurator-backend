680c8cb91bb0efd48abd8063f42669447cfb348a
frantracer <10522656+frantracer@users.noreply.github.com>
2024-10-09 10:30:09 +0200

    Set default valid domains

--- linkurator_core/infrastructure/config/env_settings.py
+++ linkurator_core/infrastructure/config/env_settings.py
@@ -5,8 +5,10 @@ from pydantic import BaseModel, AnyUrl
 
 load_dotenv()
 
+DEFAULT_DOMAINS = "linkurator.com,api.linkurator.com,www.linkurator.com"
+
 
 class EnvSettings(BaseModel):
     GOOGLE_SECRET_PATH: str = os.getenv('LINKURATOR_GOOGLE_SECRET_PATH', "secrets/client_secret.json")
     WEBSITE_URL: AnyUrl = AnyUrl(os.getenv('LINKURATOR_WEBSITE_URL', "https://linkurator.com"))
-    VALID_DOMAINS: list[str] = os.getenv('LINKURATOR_VALID_DOMAINS', "linkurator.com").split(",")
+    VALID_DOMAINS: list[str] = os.getenv('LINKURATOR_VALID_DOMAINS', DEFAULT_DOMAINS).split(",")
