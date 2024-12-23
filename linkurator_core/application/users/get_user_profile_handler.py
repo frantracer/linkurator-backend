5d932b3df5c2d9d58981b770b228e659b301c930
frantracer <10522656+frantracer@users.noreply.github.com>
2024-12-23 23:20:21 +0100

    Update last login at when retrieving profile

--- linkurator_core/application/users/get_user_profile_handler.py
+++ linkurator_core/application/users/get_user_profile_handler.py
@@ -1,6 +1,8 @@
-from typing import Optional
+from datetime import datetime, timedelta
+from typing import Callable, Optional
 from uuid import UUID
 
+from linkurator_core.domain.common.utils import datetime_now
 from linkurator_core.domain.users.user import User
 from linkurator_core.domain.users.user_repository import UserRepository
 
@@ -11,5 +13,12 @@ class GetUserProfileHandler:
     def __init__(self, user_repository: UserRepository) -> None:
         self.user_repository = user_repository
 
-    async def handle(self, user_id: UUID) -> Optional[User]:
-        return await self.user_repository.get(user_id)
+    async def handle(self, user_id: UUID, now_function: Callable[[], datetime] = datetime_now) -> Optional[User]:
+        user = await self.user_repository.get(user_id)
+
+        now = now_function()
+        if user is not None and now - user.last_login_at > timedelta(hours=1):
+            user.last_login_at = now
+            await self.user_repository.update(user)
+
+        return user
---
+++ tests/unit/test_get_user_profile_handler.py
@@ -0,0 +1,55 @@
+from datetime import timedelta
+from uuid import UUID
+import pytest
+from linkurator_core.application.users.get_user_profile_handler import GetUserProfileHandler
+from linkurator_core.domain.common.mock_factory import mock_user
+from linkurator_core.infrastructure.in_memory.user_repository import InMemoryUserRepository
+
+
+@pytest.mark.asyncio
+async def test_get_user_profile_handler() -> None:
+    user_repository = InMemoryUserRepository()
+    user = mock_user()
+    await user_repository.add(user)
+
+    handler = GetUserProfileHandler(user_repository)
+    result = await handler.handle(user.uuid)
+
+    assert result == user
+
+
+@pytest.mark.asyncio
+async def test_get_user_profile_handler_user_not_found() -> None:
+    user_repository = InMemoryUserRepository()
+    handler = GetUserProfileHandler(user_repository)
+    result = await handler.handle(UUID("1930a3ce-5f9c-4ee1-be67-6055c26cc4e2"))
+
+    assert result is None
+
+
+@pytest.mark.asyncio
+async def test_get_user_profile_handler_update_last_login_at() -> None:
+    user_repository = InMemoryUserRepository()
+    user = mock_user()
+    await user_repository.add(user)
+
+    handler = GetUserProfileHandler(user_repository)
+    current_time = user.last_login_at + timedelta(minutes=61)
+    result = await handler.handle(user_id=user.uuid, now_function=lambda: current_time)
+
+    assert result is not None
+    assert result.last_login_at == current_time
+
+
+@pytest.mark.asyncio
+async def test_get_user_profile_handler_does_not_update_last_login_if_less_than_hour_passed() -> None:
+    user_repository = InMemoryUserRepository()
+    user = mock_user()
+    await user_repository.add(user)
+
+    handler = GetUserProfileHandler(user_repository)
+    current_time = user.last_login_at + timedelta(minutes=59)
+    result = await handler.handle(user_id=user.uuid, now_function=lambda: current_time)
+
+    assert result is not None
+    assert result.last_login_at == user.last_login_at
