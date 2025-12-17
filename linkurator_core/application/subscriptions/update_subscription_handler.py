7bf1d48fd0fc928b870de09baf567008097c235f
frantracer <frantracer@gmail.com>
2025-12-17 19:05:17 +0100

    Update update_at time when updating a subscription

--- linkurator_core/application/subscriptions/update_subscription_handler.py
+++ linkurator_core/application/subscriptions/update_subscription_handler.py
@@ -2,6 +2,8 @@ from uuid import UUID
 
 from linkurator_core.domain.common.event import SubscriptionNeedsSummarizationEvent
 from linkurator_core.domain.common.event_bus_service import EventBusService
+from linkurator_core.domain.common.types import DateGenerator
+from linkurator_core.domain.common.utils import datetime_now
 from linkurator_core.domain.subscriptions.subscription_repository import SubscriptionRepository
 from linkurator_core.domain.subscriptions.subscription_service import SubscriptionService
 
@@ -11,10 +13,12 @@ class UpdateSubscriptionHandler:
                  subscription_repository: SubscriptionRepository,
                  subscription_service: SubscriptionService,
                  event_bus: EventBusService,
+                 date_generator: DateGenerator = datetime_now,
                  ) -> None:
         self.subscription_repository = subscription_repository
         self.subscription_service = subscription_service
         self.event_bus = event_bus
+        self.date_generator = date_generator
 
     async def handle(self, subscription_id: UUID) -> None:
         current_sub = await self.subscription_repository.get(subscription_id)
@@ -25,6 +29,7 @@ class UpdateSubscriptionHandler:
         if updated_sub is None:
             return
 
+        updated_sub.updated_at = self.date_generator()
         await self.subscription_repository.update(updated_sub)
 
         if current_sub.description != updated_sub.description:
